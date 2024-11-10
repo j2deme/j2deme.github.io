cbebeabaeef9a00bda4bc2d3831837613ab470fc
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-11-09 23:12:42 -0600

    Include Preact on Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://j2deme.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), preact({ compat: true })],
   markdown: {
     remarkPlugins: [remarkModifiedTime],
   },
