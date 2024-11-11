62d94ab00b626f7f4f505525fbbdd1b353e66ecd
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-11-10 19:40:09 -0600

    Add rehype-pretty-code and astro-icon dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,41 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 
+import { transformerCopyButton } from "@rehype-pretty/transformers";
+import {
+  transformerMetaHighlight,
+  transformerNotationDiff,
+} from "@shikijs/transformers";
+import rehypePrettyCode from "rehype-pretty-code";
+
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://j2deme.github.io",
-  integrations: [mdx(), sitemap(), preact({ compat: true })],
+  integrations: [mdx(), sitemap(), preact({ compat: true }), icon()],
   markdown: {
+    syntaxHighlight: false,
     remarkPlugins: [remarkModifiedTime],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: {
+            light: "catppuccin-frappe",
+            dark: "catppuccin-latte",
+          },
+          transformers: [
+            transformerNotationDiff(),
+            transformerMetaHighlight(),
+            transformerCopyButton({
+              visibility: "hover",
+              feedbackDuration: 1000,
+            }),
+          ],
+        },
+      ],
+    ],
   },
   trailingSlash: "always",
 });
